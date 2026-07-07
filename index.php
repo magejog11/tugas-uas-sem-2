426ed6088c378eb77eaf143784bd0da440cd17c7
magelangjogja11-bit <magelangjogja11@gmail.com>
2026-07-07 20:37:20 +0700

    add index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $result = $conn->query($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forum Diskusi Online</title>
+    <title>FORKOM UIM</title>
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
     <!-- Bootstrap 5 CSS CDN -->
