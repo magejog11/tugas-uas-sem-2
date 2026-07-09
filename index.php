af9fef848a961c6e295896eae88507703b526635
Your Name <you@example.com>
2026-07-10 01:58:08 +0700

    ganti nama

--- index.php
+++ index.php
@@ -580,7 +580,7 @@ $result = $conn->query($query);
                 </a>
                 <div class="header-search">
                     <i class="fas fa-search"></i>
-                    <input type="text" placeholder="Cari di DIPSEN..." aria-label="Cari" />
+                    <input type="text" placeholder="Cari di TIPSEN..." aria-label="Cari" />
                 </div>
             </div>
 
